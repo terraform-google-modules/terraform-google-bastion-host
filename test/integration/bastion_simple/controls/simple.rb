cdcaad9b91c2af4bffc4aa18fe30c7ef5878098c
Ryan Canty <jrcanty@gmail.com>
2019-11-26 18:13:19 -0800

    Added more inspec tests including SSH

--- test/integration/bastion_simple/controls/simple.rb
+++ test/integration/bastion_simple/controls/simple.rb
@@ -12,37 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+HOSTNAME = 'bastion-vm'
+ZONE = 'us-west1-a'
+
 project_id = attribute('project_id')
 
 control "Bastion Instance" do
   title "Simple Configuration"
-
-  describe command("gcloud --project=#{project_id} compute instances list --format=json --filter='name~bastion*'") do
+  describe "Instance configuration" do
+    subject { command("gcloud --project=#{project_id} compute instances describe #{HOSTNAME} --format=json --zone #{ZONE}") }
     its(:exit_status) { should eq 0 }
     its(:stderr) { should eq '' }
+    let!(:data) { JSON.parse(subject.stdout) if subject.exit_status == 0 }
 
-    let!(:data) do
-      if subject.exit_status == 0
-        JSON.parse(subject.stdout)
-      else
-        []
-      end
+    it "should be running" do
+      expect(data['status']).to eq('RUNNING')
     end
 
-    describe "number of instances" do
-      it "should be 1" do
-        expect(data.length).to eq(1)
-      end
+    it "should have correct OS Login config" do
+      entry = data['metadata']['items'].find { |i| i['key'] == "enable-oslogin" }
+      expect(entry['value']).to eq('TRUE')
     end
 
-    describe "instance" do
-      let(:instance) do
-        data.find { |i| i['name'] == "bastion-vm" }
-      end
-
-      it "should be in zone us-west1-a}" do
-        expect(instance['zone']).to match(/.*us-west1-a$/)
+    it "should have Shielded VM enabled" do
+      %w(enableIntegrityMonitoring enableSecureBoot enableVtpm).each do |i|
+        expect(data['shieldedInstanceConfig'][i]).to eq(true)
       end
     end
   end
+
+  describe "SSH Firewall Rule" do
+    subject { command("gcloud --project #{project_id} compute firewall-rules describe allow-ssh-from-iap-to-tunnel --format json") }
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should eq '' }
+    let!(:data) { JSON.parse(subject.stdout) if subject.exit_status == 0 }
+
+    it "should allow SSH ingress from IAP" do
+      expect(data['sourceRanges'][0]).to eq('35.235.240.0/20')
+      expect(data['direction']).to eq('INGRESS')
+      expect(data['targetServiceAccounts'][0]).to match("bastion@#{project_id}.iam.gserviceaccount.com")
+      expect(data['allowed'][0]['ports'][0]).to eq("22")
+    end
+  end
+
 end
