863a1216d0502d1fc15ef657f7a8990c26892d96
coffee coder <64681762+c0ffeec0der@users.noreply.github.com>
2020-05-09 22:45:18 +0700

    Expose disk size and type. Default 10gb pd-standard (#39)
    * Expose disk size and type. Default 10gb pd-standard
    
    * Expose disk size and type. Default 100gb pd-standard
    
    * Expose disk size and type. Default 100gb pd-standard
    
    Co-authored-by: c0feec0der <>

--- main.tf
+++ main.tf
@@ -49,6 +49,8 @@ module "instance_template" {
   name_prefix  = var.name_prefix
   project_id   = var.project
   machine_type = var.machine_type
+  disk_size_gb = var.disk_size_gb
+  disk_type    = var.disk_type
   subnetwork   = var.subnet
   service_account = {
     email  = local.service_account_email
