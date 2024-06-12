ec301c14f1814eb07137f7881f1296f78ff6bf23
Marcus Ramberg <marcus@means.no>
2024-06-12 14:49:28 +0200

    feat: Support setting a static ip for the bastion

--- main.tf
+++ main.tf
@@ -91,6 +91,7 @@ resource "google_compute_instance_from_template" "bastion_vm" {
     subnetwork         = var.subnet
     subnetwork_project = var.host_project != "" ? var.host_project : var.project
     access_config      = var.external_ip ? var.access_config : []
+    network_ip         = var.network_ip
   }
 
   source_instance_template = module.instance_template.self_link
