c0b5acf3ca51a4917010f0095621d6743f2b4689
Andrew Peabody <andrewpeabody@google.com>
2024-08-29 08:59:45 -0700

    fix: set empty access_config to null (#206)

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "google_compute_instance_from_template" "bastion_vm" {
   network_interface {
     subnetwork         = var.subnet
     subnetwork_project = var.host_project != "" ? var.host_project : var.project
-    access_config      = var.external_ip ? var.access_config : []
+    access_config      = var.external_ip ? var.access_config : null
     network_ip         = var.network_ip
   }
 
